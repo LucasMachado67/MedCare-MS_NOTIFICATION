4c8e8ff185e625b72e001248b5efc83e9bc62216
lucasem67 <lucasem80@gmail.com>
2026-02-04 08:38:14 -0300

    arrumando bugs e removendo RabbitMQ

--- src/main/java/com/ms/notification/consumers/EmailConsumer.java
+++ src/main/java/com/ms/notification/consumers/EmailConsumer.java
@@ -11,15 +11,18 @@ import com.ms.notification.models.Email;
 import com.ms.notification.services.EmailService;
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import org.springframework.stereotype.Component;
+
 /**
  * Consumer responsável por receber mensagens de e-mail provenientes do broker
  * RabbitMQ. Quando um evento chega na fila configurada, os dados são
- * convertidos em um modelo {@link Email} e encaminhados para o serviço de envio.
+ * convertidos num modelo {@link Email} e encaminhados para o serviço de envio.
  *
  * Esta classe atua como ponto de integração entre a mensageria e o serviço de
  * notificação, garantindo que cada mensagem recebida represente uma tentativa
  * válida de envio de e-mail.
  */
+@Component
 public class EmailConsumer {
 
     @Autowired
@@ -36,14 +39,10 @@ public class EmailConsumer {
      * dados do DTO para o modelo {@link Email}, preservando o padrão de dados
      * esperado pelo serviço de envio.
      * </p>
-     *
-     * @param dto Objeto recebido da fila contendo os dados necessários para
-     *            a construção e envio do e-mail.
-     * @throws JsonProcessingException 
-     * @throws JsonMappingException 
      */
     @SqsListener(value = "${medcare.aws.sqs.queue.notification.email}")
     public void listenEmailQueue(String payload){
+        System.out.println("Received Message" + payload);
         try {
             Email emailModel = new Email();
             EmailRecordDto event =
